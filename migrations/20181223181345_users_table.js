596f2fc7dfde4723b61fdbc1f6b3f4ae9972d272
Bobby Bryant <bobby@developwithwp.com>
2018-12-24 15:10:38 -0500

    Update users table

--- migrations/20181223181345_users_table.js
+++ migrations/20181223181345_users_table.js
@@ -1,8 +1,14 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', table => {
     table.increments('id').primary();
-    table.unique('email');
-    table.string('password');
+    table
+      .string('email')
+      .unique()
+      .notNullable();
+    table.string('password').notNullable();
+    table.string('first_name');
+    table.string('last_name');
+    table.enu('tole', ['admin', 'author', 'customer']);
     table.timestamps(false, true);
     table.timestamp('deleted_at').defaultTo(knex.fn.now());
   });
