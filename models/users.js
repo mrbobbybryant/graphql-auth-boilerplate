b74868ccc6bd97147b4ade8b0ff202e17bd9f217
Bobby Bryant <bobby@developwithwp.com>
2018-12-25 21:34:21 -0500

    Create more User helper methods

--- models/users.js
+++ models/users.js
@@ -16,10 +16,24 @@ export const get = async email => {
     .first();
 };
 
+export const getUserById = async id =>
+  User.query()
+    .where({ id })
+    .first();
+
 export const getAll = async () => {
   return await User.query();
+};
+
+export const update = async (id, data) => {
+  console.log(id);
+  return await User.query()
+    .where({ id })
+    .update(data);
 };
 
 User.create = create;
 User.get = get;
 User.getAll = getAll;
+User.update = update;
+User.getUserById = getUserById;
